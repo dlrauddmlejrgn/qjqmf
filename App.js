70e4bf1f055cd93a83fa44865d08d8593abe41d6
dlrauddmlejrgn <124484788+dlrauddmlejrgn@users.noreply.github.com>
2026-07-16 04:42:44 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import 'react-native-gesture-handler';
 
 import Artists from './assets/Artists.json'
 
